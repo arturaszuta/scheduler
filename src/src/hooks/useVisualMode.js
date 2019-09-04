0cb187ae11abefa9b0d4ce1965ad62aca1c56788
Arturas Zuta <arturaszuta@gmail.com>
2019-09-04 13:48:44 -0400

    Add visual state

---
+++ src/src/hooks/useVisualMode.js
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+
+  function transition(mode, replace = false) {
+    if (!replace) {
+      setHistory([...history, mode]);
+      setMode(mode);
+    } else {
+      setMode(mode);  
+    }
+  }
+  
+  function back() {
+    if (history.length > 1) {
+      const previousHistory = history.slice(0,-1);
+      setHistory(previousHistory);
+      setMode(previousHistory[previousHistory.length - 1]);
+    } else {
+      setMode(history[0]);
+    }
+  }
+  
+
+  return {
+    mode,
+    transition,
+    back 
+  }
+}
+
+// useVisualMode("FIRST");
+// useVisualMode.transition("SECOND");
+// useVisualMode.transition("THIRD");
