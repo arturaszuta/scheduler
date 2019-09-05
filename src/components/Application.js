841369e3784de0073235079f3c9621988023e611
Arturas Zuta <arturaszuta@gmail.com>
2019-09-05 11:54:18 -0400

    Add separate file to handle application data

--- src/components/Application.js
+++ src/components/Application.js
@@ -7,6 +7,7 @@ import DayList from "./DayList";
 import Appointment from "./Appointment/index"
 import { promised } from "q";
 import { getAppointmentsForDay, getInterviewer, getInterviewersForDay } from "../helpers/selectors";
+import useApplicationData from "../src/hooks/useApplicationData";
 
 
 
@@ -14,30 +15,15 @@ import { getAppointmentsForDay, getInterviewer, getInterviewersForDay } from "..
 
 
 export default function Application(props) {
-  
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: {}
-  });
 
-  const setDay = (day) => {
-    setState({...state, day: day})
-  }
-  
-  useEffect(() => {
+  const {
+    state,
+    setDay,
+    bookInterview,
+    deleteInterview
+  } = useApplicationData();
 
-    Promise.all([
-        axios.get('http://localhost:8001/api/days'),
-        axios.get('http://localhost:8001/api/appointments'),
-        axios.get('http://localhost:8001/api/interviewers'),
-      ]).then((all) => {
-        console.log('THI IS BEING CALLED');
-        setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }))
-      })
 
-    }, [])
 
     let allAppointments = getAppointmentsForDay(state, state.day).map((item) => {
       let interview = null;
@@ -46,29 +32,7 @@ export default function Application(props) {
       }
     const allInterviewers = getInterviewersForDay(state, state.day);
     
-    function bookInterview(id, interview) {
-      const appointment = {
-        ...state.appointments[id],
-        interview: { ...interview }
-      };
-      const appointments = {
-        ...state.appointments,
-        [id]: appointment
-      };
-      setState({...state, appointments});
-    }
-
-    function deleteInterview(id, interview) {
-      const appointment = {
-        ...state.appointments[id],
-        interview: { interview }
-      };
-      const appointments = {
-        ...state.appointments,
-        [id]: appointment
-      };
-      setState({...state, appointments});
-    }
+    
  
   return <Appointment key={item.id} 
   {...item} 
